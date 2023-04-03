92da157175223de34c0c895fd7a80680000491dc
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2023-04-02 21:36:16 -0300

    Desafio Final

--- Entrega Final/script-creacion.sql
+++ Entrega Final/script-creacion.sql
@@ -329,37 +329,37 @@ DROP TRIGGER IF EXISTS before_ventas_insert;
 CREATE 
     TRIGGER  before_ventas_insert
  BEFORE INSERT ON ventas FOR EACH ROW 
-    INSERT INTO before_ventas_log SET id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Insert';
+    INSERT INTO before_ventas_log SET Id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Insert';
 
 DROP TRIGGER IF EXISTS before_ventas_update;
 CREATE 
     TRIGGER  before_ventas_update
  BEFORE UPDATE ON ventas FOR EACH ROW 
-    INSERT INTO before_ventas_log SET id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Update';
+    INSERT INTO before_ventas_log SET Id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Update';
 
 DROP TRIGGER IF EXISTS before_ventas_delete;
 CREATE 
     TRIGGER  before_ventas_delete
  BEFORE DELETE ON ventas FOR EACH ROW 
-    INSERT INTO before_ventas_log SET id_registro = OLD.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Delete';
+    INSERT INTO before_ventas_log SET Id_registro = OLD.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Delete';
 
 DROP TRIGGER IF EXISTS after_ventas_insert;
 CREATE 
     TRIGGER  after_ventas_insert
  AFTER INSERT ON ventas FOR EACH ROW 
-    INSERT INTO after_ventas_log SET id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Insert';
+    INSERT INTO after_ventas_log SET Id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Insert';
 
 DROP TRIGGER IF EXISTS after_ventas_update;
 CREATE 
     TRIGGER  after_ventas_update
  AFTER UPDATE ON ventas FOR EACH ROW 
-    INSERT INTO after_ventas_log SET id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Update';
+    INSERT INTO after_ventas_log SET Id_registro = NEW.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Update';
 
 DROP TRIGGER IF EXISTS after_ventas_delete;
 CREATE 
     TRIGGER  after_ventas_delete
  AFTER DELETE ON ventas FOR EACH ROW 
-    INSERT INTO after_ventas_log SET id_registro = OLD.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Delete';
+    INSERT INTO after_ventas_log SET Id_registro = OLD.Id_venta , nombre_usuario = USER() , fecha = CURDATE() , hora = CURTIME() , operacion = 'Delete';
     
 -- Creacion de Funciones
 
